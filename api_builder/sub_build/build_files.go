4609b330d84b3a1c80125ec297e99bc92d456c11
Nick <64551534+null-nick@users.noreply.github.com>
2024-02-16 00:59:49 +0100

    Fixed some issue with thumbnail

--- api_builder/sub_build/build_files.go
+++ api_builder/sub_build/build_files.go
@@ -54,9 +54,9 @@ func BuildFiles[Scheme interfaces.SchemeInterface](typeScheme Scheme, builder *c
 					builder.SetVarValue(fmt.Sprintf("files[\"%s\"]", field.Name), fmt.Sprintf("entity.%s", prettifiedField)).AddLine()
 				}
 				if isMethod {
-					if field.Name == "thumb" {
+					if field.Name == "thumbnail" {
 						builder.AddImport("", fmt.Sprintf("%s/types", consts.PackageName))
-						builder.SetVarValue(fmt.Sprintf("entity.%s", prettifiedField), "types.InputURL(\"attach://thumb\")").AddLine()
+						builder.SetVarValue(fmt.Sprintf("entity.%s", prettifiedField), "types.InputURL(\"attach://thumbnail\")").AddLine()
 					} else {
 						builder.SetVarValue(fmt.Sprintf("entity.%s", prettifiedField), "nil").AddLine()
 					}
@@ -72,8 +72,8 @@ func BuildFiles[Scheme interfaces.SchemeInterface](typeScheme Scheme, builder *c
 				builder.InitVarValue("x1", "x0.(rawTypes.InputMediaFiles).Files()").AddLine()
 				builder.AddFor("k, v := range x1")
 				builder.DeclareVar("attachName", "string").AddLine()
-				builder.AddIf("k == \"thumb\"")
-				builder.SetVarValue("attachName", "fmt.Sprintf(\"file-%d-thumb\", i)").AddLine()
+				builder.AddIf("k == \"thumbnail\"")
+				builder.SetVarValue("attachName", "fmt.Sprintf(\"file-%d-thumbnail\", i)").AddLine()
 				builder.CallFunction("x0.SetAttachmentThumb", []string{"attachName"}).AddLine()
 				builder.AddElse()
 				builder.SetVarValue("attachName", "fmt.Sprintf(\"file-%d\", i)").AddLine()
@@ -88,7 +88,7 @@ func BuildFiles[Scheme interfaces.SchemeInterface](typeScheme Scheme, builder *c
 				}
 				builder.AddFor(fmt.Sprintf("k, v := range entity.%s.Files()", tmpField))
 				builder.SetVarValue("files[k]", "v").AddLine()
-				builder.AddIf("k == \"thumb\"")
+				builder.AddIf("k == \"thumbnail\"")
 				builder.CallFunction("entity.Media.SetAttachmentThumb", []string{"k"}).AddLine()
 				builder.AddElse()
 				builder.CallFunction("entity.Media.SetAttachment", []string{"k"}).AddLine()
@@ -106,14 +106,14 @@ func BuildFiles[Scheme interfaces.SchemeInterface](typeScheme Scheme, builder *c
 			builder.CloseBracket().AddLine()
 			var foundThumb bool
 			for _, field := range typeScheme.GetFields() {
-				if field.Name == "thumb" {
+				if field.Name == "thumbnail" {
 					foundThumb = true
 					break
 				}
 			}
 			if foundThumb {
 				builder.AddFunc("entity "+structName, "SetAttachmentThumb", []string{"attach string"}, "")
-				builder.SetVarValue("entity.Thumb", "InputURL(fmt.Sprintf(\"attach://%s\", attach))").AddLine()
+				builder.SetVarValue("entity.Thumbnail", "InputURL(fmt.Sprintf(\"attach://%s\", attach))").AddLine()
 			} else {
 				builder.AddFunc("entity "+structName, "SetAttachmentThumb", []string{"_ string"}, "")
 			}
